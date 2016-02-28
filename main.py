819c8bd3b7c7930c4030aa66f0675685b84270e8
mdczaplicki <mdczaplicki@gmail.com>
2016-02-28 01:59:50 +0100

    Commit #2

--- main.py
+++ main.py
@@ -11,14 +11,16 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     with open('series.cfg', 'r') as file:
         config.read_file(file)
-        for serie in config.options('series'):
-            metadata = get_metadata(serie)
-            series[serie] = {RELEASE_DATE: metadata[RELEASE_DATE],
-                             TITLE: metadata['show'][TITLE],
-                             SEASON: metadata[SEASON],
-                             EPISODE: metadata[EPISODE]}
+        for show in config.options('series'):
+            metadata = get_metadata(show)
+            series[show] = {RELEASE_DATE: metadata[RELEASE_DATE],
+                            TITLE: metadata['show'][TITLE],
+                            SEASON: metadata[SEASON],
+                            EPISODE: metadata[EPISODE]}
         file.close()
     del config
+    for show, data in series.items():
+        pass
     delay = days_between(today, series['the100'][RELEASE_DATE])
     data = PluralDict({'title': 'ABC', 'season': '01', 'episode': '03', 'days': 2})
     print(MB_MESSAGE['en_EN'].format_map(data))
